a949facae3338f019a98b6f6ff2254eaeb5bb045
Tomás Abril <tomasabril@users.noreply.github.com>
2014-10-14 20:08:59 -0300

    fix bugs

--- aula2.cpp
+++ aula2.cpp
@@ -1,61 +1,56 @@
 /* entrega dia 21/10/14
 Criar um programa para manipular uma lista duplamente encadeada
-o O programa dever· apresentar um menu com as seguintes opÁıes:
-
-1. Inclus„o no inÌcio da lista
-2. Inclus„o no fim da lista
-3. RemoÁ„o do inÌcio da lista
-4. RemoÁ„o do fim da lista
-5. Impress„o da lista com valor do topo, e todos os nÛs, contendo
-o endereÁo do nÛ, valor, endereÁo do ponteiro direito e do
+o O programa dever√° apresentar um menu com as seguintes op√ß√µes:
+1. Inclus√£o no in√≠cio da lista
+2. Inclus√£o no fim da lista
+3. Remo√ß√£o do in√≠cio da lista
+4. Remo√ß√£o do fim da lista
+5. Impress√£o da lista com valor do topo, e todos os n√≥s, contendo
+o endere√ßo do n√≥, valor, endere√ßo do ponteiro direito e do
 ponteiro esquerdo.
 */
-///Tom·s Abril
+///Tom√°s Abril
 ///Allan Patrick
-
 #include <stdio.h>
 #include <iostream>
-
 using namespace std;
-
 int incluiIni();
 int incluiFim();
 int removeIni();
 int removeFim();
 int imprimeLista();
-
-struct No {
+struct No
+{
     int dado;
     No* prox;
     No* ant;
 };
-
-struct Head {
+struct Head
+{
     int tamanho;
     No* inicio;
     No* fim;
 };
-
 Head * head = new Head;
-
 int main()
 {
     int op;
     cout << "0 - termina\n";
     cout << "1 - inclusao no inicio da lista\n";
     cout << "2 - inclusao no fim da lista\n";
-    cout << "3 - remoÁ„o do inicio da lista\n";
-    cout << "4 - remoÁ„o do fim da lista\n";
-    cout << "5 - Impress„o da lista\n";
-
+    cout << "3 - remo√ß√£o do inicio da lista\n";
+    cout << "4 - remo√ß√£o do fim da lista\n";
+    cout << "5 - Impress√£o da lista\n";
     head->inicio = NULL;
     head->fim = NULL;
     head->tamanho = 0;
     int t = 1;
-    while(t) {
-        cout << "\ndigite a opÁ„o: ";
+    while(t)
+    {
+        cout << "\ndigite a op√ß√£o: ";
         cin >> op;
-        switch(op) {
+        switch(op)
+        {
         case 0 :
             t = 0;
             break;
@@ -75,102 +70,115 @@ int main()
             imprimeLista();
             break;
         default:
-            cout << "opÁ„o invalida\n";
+            cout << "op√ß√£o invalida\n";
         }
     }
-
+    delete head;
     return 0;
 }
-
 int incluiIni()
 {
     int valor;
     cout << "digite valor\n";
     cin >> valor;
-
     No * no = new No;
     no->dado = valor;
-
-    if (head->tamanho == 0) {
+    if (head->tamanho == 0)
+    {
         head->fim = no;
         head->tamanho = 1;
-    } else {
+        no->prox = NULL;
+    }
+    else
+    {
         head->tamanho ++;
         no->prox = head->inicio;
         (head->inicio)->ant = no;
     }
+    no->ant = NULL;
     head->inicio = no;
 }
-
 int incluiFim()
 {
     int valor;
     cout << "digite valor\n";
     cin >> valor;
-
     No * no = new No;
     no->dado = valor;
-
-    if (head->tamanho == 0) {
+    if (head->tamanho == 0)
+    {
         head->inicio = no;
         head->tamanho = 1;
-    } else {
+        no->ant = NULL;
+    }
+    else
+    {
         head->tamanho ++;
         no->ant = head->fim;
         (head->fim)->prox = no;
     }
+    no->prox = NULL;
     head->fim = no;
 }
-
 int removeIni()
 {
-    if(head->tamanho > 1) {
+    if(head->tamanho > 1)
+    {
         head->inicio = (head->inicio)->prox;
         delete (head->inicio)->ant;
         head->tamanho --;
-    } else if(head->tamanho == 1) {
+        (head->inicio)->ant = NULL;
+    }
+    else if(head->tamanho == 1)
+    {
         delete head->inicio;
         head->inicio = NULL;
         head->fim = NULL;
         head->tamanho = 0;
-    } else {
-        cout << "Lista j· est· vazia\n";
+    }
+    else
+    {
+        cout << "Lista j√° est√° vazia\n";
     }
 }
-
 int removeFim()
 {
-    if(head->tamanho > 1) {
+    if(head->tamanho > 1)
+    {
         head->fim = (head->fim)->ant;
         delete (head->fim)->prox;
         head->tamanho --;
-    } else if(head->tamanho == 1) {
+        (head->inicio)->prox = NULL;
+    }
+    else if(head->tamanho == 1)
+    {
         delete head->inicio;
         head->inicio = NULL;
         head->fim = NULL;
         head->tamanho = 0;
-    } else {
-        cout << "Lista j· est· vazia\n";
+    }
+    else
+    {
+        cout << "Lista j√° est√° vazia\n";
     }
 }
-
 int imprimeLista()
 {
-    cout << "\n Header\n";
-    cout << "tamanho: " << head->tamanho << "\ninicio: " << head->inicio
-         << "\nfim:    " << head->fim;
-    cout << "\n NÛs\n";
-    if(head->tamanho == 0) {
-        cout << "\n nao existe nenhum no\n";
+    cout << "\n Header --------\n";
+    cout << "\ntamanho: " << head->tamanho << "\ninicio: " << head->inicio
+         << "\nfim: " << head->fim;
+    cout << "\n\n N√≥s    --------";
+    if(head->tamanho == 0)
+    {
+        cout << "\n nao existe nenhum n√≥\n";
     }
     No* posatual = new No;
     posatual = head->inicio;
-    while(posatual != head->fim) {
-        cout << "\ndado: " << posatual->dado << "\nproximo:  " << posatual->prox
+    while(posatual != NULL)
+    {
+        cout << "\n\ndado: " << posatual->dado << "\nproximo: " << posatual->prox
              << "\nanterior: " << posatual->ant;
         posatual = posatual->prox;
     }
     delete posatual;
 }
-
-
